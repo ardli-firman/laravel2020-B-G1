065175efb7c4734c4bd91e31a6f6e4d7a55c6b88
ifmystery <enbuss88@gmail.com>
2020-03-30 02:21:07 +0700

    Revisi Fix UI Mhs

--- resources/views/mahasiswa/dashboard.blade.php
+++ resources/views/mahasiswa/dashboard.blade.php
@@ -12,7 +12,7 @@
         
 <div class="row">
     <div class="col">
-        <h5 class="card-title text-uppercase text-muted mb-0 text-black">Total Judul TA</h5>
+        <h5 class="card-title text-uppercase text-muted mb-0 text-black">Total TA Semua Mahasiswa</h5>
         <span class="h2 font-weight-bold mb-0 text-black">{{$judulta}}</span>
     </div>
     <div class="col-auto">
