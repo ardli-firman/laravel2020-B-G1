c8ef8f2d864be38692fad7b7e72d850a7bc75c8a
ardli-firman <ardlifirman17@gmail.com>
2020-04-04 00:16:37 +0700

    setup yajra datatables uwu

--- config/app.php
+++ config/app.php
@@ -174,6 +174,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Yajra Datatables
+        Yajra\DataTables\DataTablesServiceProvider::class,
     ],
 
     /*
@@ -224,7 +226,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        // Yajra Datatables
+        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
     ],
 
 ];
